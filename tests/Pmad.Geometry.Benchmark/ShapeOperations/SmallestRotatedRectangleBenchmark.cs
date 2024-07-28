e189c6fef4f692a25e89d17fa545a0e358bd7714
Julien <julien.etelain@outlook.com>
2024-07-28 11:15:01 +0200

    Remove unused code

--- tests/Pmad.Geometry.Benchmark/ShapeOperations/SmallestRotatedRectangleBenchmark.cs
+++ tests/Pmad.Geometry.Benchmark/ShapeOperations/SmallestRotatedRectangleBenchmark.cs
@@ -7,15 +7,16 @@ namespace Pmad.Geometry.Benchmark.ShapeOperations
     public class SmallestRotatedRectangleBenchmark
     {
         [Benchmark]
-        public void Vector2D_StaticOperators() => RotatedRectangle<double, Vector2D>.GetSmallestContaining(SampleValuesRO.RandomList2D);
+        public void Vector2D() => RotatedRectangle<double, Vector2D>.GetSmallestContaining(SampleValuesRO.RandomList2D);
 
         [Benchmark]
-        public void Vector2D_Direct() => SmallestRotatedRectangle.Compute(ShapeSettings<double, Vector2D>.Default, SampleValuesRO.RandomList2D.AsSpan());
+        public void Vector2F() => RotatedRectangle<float, Vector2F>.GetSmallestContaining(SampleValuesRO.RandomList2F);
 
         [Benchmark]
-        public void Vector2F_StaticOperators() => RotatedRectangle<float, Vector2F>.GetSmallestContaining(SampleValuesRO.RandomList2F);
+        public void Vector2DS() => RotatedRectangle<double, Vector2DS>.GetSmallestContaining(SampleValuesRO.RandomList2DS);
 
         [Benchmark]
-        public void Vector2F_Direct() => SmallestRotatedRectangle.Compute(ShapeSettings<float, Vector2F>.Default, SampleValuesRO.RandomList2F.AsSpan());
+        public void Vector2FS() => RotatedRectangle<float, Vector2FS>.GetSmallestContaining(SampleValuesRO.RandomList2FS);
+
     }
 }
