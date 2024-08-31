a567acad9df10f3d14c70defb1c27b61874783ba
Julien <julien.etelain@outlook.com>
2024-08-31 12:04:42 +0200

    Improve clipper conversion operations performance

---
+++ tests/Pmad.Geometry.Benchmark/PolygonSampleSet.cs
@@ -0,0 +1,65 @@
+﻿using Clipper2Lib;
+using GameRealisticMap.Geometries;
+using NetTopologySuite.Geometries;
+using Pmad.Geometry.Collections;
+
+namespace Pmad.Geometry.Benchmark
+{
+    public class PolygonSampleSet
+    {
+        public static PolygonSampleSet CreateCircle(int radius)
+            => CreateCircle(0, 0, radius);
+
+        public static PolygonSampleSet CreateCircle(int x, int y, int radius) 
+            => new PolygonSampleSet(Enumerable.Range(0, 41).Select(a => new Vector2I((int)(Math.Cos(a * Math.PI / 20) * radius) + x, (int)(Math.Sin(a * Math.PI / 20) * radius) + y)));
+
+        public PolygonSampleSet(IEnumerable<Vector2I> source)
+        {
+            Points2I = source.ToReadOnlyArray();
+            Points2F = Points2I.Select(p => new Vector2F(p.X, p.Y)).ToReadOnlyArray();
+            Points2D = Points2I.Select(p => new Vector2D(p.X, p.Y)).ToReadOnlyArray();
+            Points2L = Points2I.Select(p => new Vector2L(p.X, p.Y)).ToReadOnlyArray();
+            Points2IS = Points2I.Select(p => new Vector2IS(p.X, p.Y)).ToReadOnlyArray();
+            Points2FS = Points2I.Select(p => new Vector2FS(p.X, p.Y)).ToReadOnlyArray();
+            Points2DS = Points2I.Select(p => new Vector2DS(p.X, p.Y)).ToReadOnlyArray();
+            Points2LS = Points2I.Select(p => new Vector2LS(p.X, p.Y)).ToReadOnlyArray();
+            PointsP64 = new Path64(Points2I.Select(p => new Point64(p.X, p.Y)));
+            PointsGRM = Points2I.Select(p => new TerrainPoint(p.X, p.Y)).ToList();
+            PointsNTS = Points2I.Select(p => new Coordinate(p.X, p.Y)).ToArray();
+
+            Polygon2I = new(Points2I);
+            Polygon2F = new(Points2F);
+            Polygon2D = new(Points2D);
+            Polygon2L = new(Points2L);
+            Polygon2IS = new(Points2IS);
+            Polygon2FS = new(Points2FS);
+            Polygon2DS = new(Points2DS);
+            Polygon2LS = new(Points2LS);
+            PolygonGRM = new(PointsGRM);
+            PolygonNTS = new(new(PointsNTS));
+        }
+
+        public readonly ReadOnlyArray<Vector2I> Points2I;
+        public readonly ReadOnlyArray<Vector2F> Points2F;
+        public readonly ReadOnlyArray<Vector2D> Points2D;
+        public readonly ReadOnlyArray<Vector2L> Points2L;
+        public readonly ReadOnlyArray<Vector2IS> Points2IS;
+        public readonly ReadOnlyArray<Vector2FS> Points2FS;
+        public readonly ReadOnlyArray<Vector2DS> Points2DS;
+        public readonly ReadOnlyArray<Vector2LS> Points2LS;
+        public readonly Path64 PointsP64;
+        public readonly List<TerrainPoint> PointsGRM;
+        public readonly Coordinate[] PointsNTS;
+
+        public readonly Shapes.Polygon<int, Vector2I> Polygon2I;
+        public readonly Shapes.Polygon<float, Vector2F> Polygon2F;
+        public readonly Shapes.Polygon<double, Vector2D> Polygon2D;
+        public readonly Shapes.Polygon<long, Vector2L> Polygon2L;
+        public readonly Shapes.Polygon<int, Vector2IS> Polygon2IS;
+        public readonly Shapes.Polygon<float, Vector2FS> Polygon2FS;
+        public readonly Shapes.Polygon<double, Vector2DS> Polygon2DS;
+        public readonly Shapes.Polygon<long, Vector2LS> Polygon2LS;
+        public readonly TerrainPolygon PolygonGRM;
+        public readonly Polygon PolygonNTS;
+    }
+}
---
+++ tests/Pmad.Geometry.Benchmark/ShapeOperations/UnionBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Pmad.Geometry.Benchmark.ShapeOperations
+{
+    public class UnionBenchmark
+    {
+        private static readonly PolygonSampleSet A = PolygonSampleSet.CreateCircle(0, 0, 100); 
+        private static readonly PolygonSampleSet B = PolygonSampleSet.CreateCircle(0, 50, 100);
+        private static readonly PolygonSampleSet C = PolygonSampleSet.CreateCircle(0, 150, 100);
+
+        [Benchmark] public object Union_NTS() => (A.PolygonNTS.Union(B.PolygonNTS), A.PolygonNTS.Union(C.PolygonNTS));
+        [Benchmark] public object Union_GRM() => (A.PolygonGRM.Merge(B.PolygonGRM), A.PolygonGRM.Merge(C.PolygonGRM));
+        [Benchmark] public object Union_2D() => (A.Polygon2D.Union(B.Polygon2D) , A.Polygon2D.Union(C.Polygon2D));
+        [Benchmark] public object Union_2F() => (A.Polygon2F.Union(B.Polygon2F), A.Polygon2F.Union(C.Polygon2F));
+        [Benchmark] public object Union_2I() => (A.Polygon2I.Union(B.Polygon2I), A.Polygon2I.Union(C.Polygon2I));
+        [Benchmark] public object Union_2L() => (A.Polygon2L.Union(B.Polygon2L) , A.Polygon2L.Union(C.Polygon2L));
+
+    }
+}
