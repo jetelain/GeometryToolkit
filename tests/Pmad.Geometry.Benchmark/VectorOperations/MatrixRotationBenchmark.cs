408d05d6fc3dbf2011aded032cac8cc46e39ba75
Julien <julien.etelain@outlook.com>
2024-09-04 19:29:53 +0200

    Fix partial commit

--- tests/Pmad.Geometry.Benchmark/VectorOperations/MatrixRotationBenchmark.cs
+++ tests/Pmad.Geometry.Benchmark/VectorOperations/MatrixRotationBenchmark.cs
@@ -6,7 +6,7 @@ namespace Pmad.Geometry.Benchmark.VectorOperations
     public class MatrixRotationBenchmark
     {
         [Benchmark] public void Matrix2x2_Generic_2D() => SampleValuesRO.RandomList2D.ForEach(p => Matrix2x2<double, Vector2D>.CreateRotation(1).Transform(p));
-        [Benchmark] public void Matrix2x2_GenericD_2D() => SampleValuesRO.RandomList2D.ForEach(p => Matrix2x2<double, Vector2D>.CreateRotationD(1).Transform(p));
+        [Benchmark] public void Matrix2x2_GenericD_2D() => SampleValuesRO.RandomList2D.ForEach(p => Matrix2x2<double, Vector2D>.CreateRotation(1).Transform(p));
         [Benchmark] public void Matrix2x2_Specific_2D() => SampleValuesRO.RandomList2D.ForEach(p => Matrix2x2D.CreateRotation(1).Transform(p));
         //[Benchmark] public void Matrix2x2_Generic_2DS() => SampleValuesRO.RandomList2DS.ForEach(p => Matrix2x2<double, Vector2DS>.CreateRotation(1).Transform(p));
         //[Benchmark] public void Matrix2x2_Specific_2DS() => SampleValuesRO.RandomList2DS.ForEach(p => Matrix2x2DS.CreateRotation(1).Transform(p));
