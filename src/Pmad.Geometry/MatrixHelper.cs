cdfa751e6ee7fcac8ca0223ed3fa282c3967b3b8
Julien <julien.etelain@outlook.com>
2024-09-07 15:50:29 +0200

    Remove V1 code

--- src/Pmad.Geometry/MatrixHelper.cs
+++ src/Pmad.Geometry/MatrixHelper.cs
@@ -6,29 +6,6 @@ namespace Pmad.Geometry
     internal static class MatrixHelper
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static (double Sin, double Cos) SinCosV1(double radians)
-        {
-            radians = Math.IEEERemainder(radians, Math.PI * 2.0);
-            if (radians > -1.7453294E-05 && radians < 1.7453294E-05)
-            {
-                return (0, 1);
-            }
-            if (radians > 1.570779 && radians < 1.5708138)
-            {
-                return (1, 0);
-            }
-            if (radians < -3.1415753 || radians > 3.1415753)
-            {
-                return (0, -1);
-            }
-            if (radians > -1.5708138 && radians < -1.570779)
-            {
-                return (-1, 0);
-            }
-            return Math.SinCos(radians);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static (double Sin, double Cos) SinCos(double radians)
         {
             radians = radians % (Math.PI * 2);
@@ -58,30 +35,6 @@ namespace Pmad.Geometry
             }
             return Math.SinCos(radians);
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static (float Sin, float Cos) SinCosV1(float radians)
-        {
-            radians = MathF.IEEERemainder(radians, MathF.PI * 2f);
-            if (radians > -1.7453294E-05f && radians < 1.7453294E-05f)
-            {
-                return (0, 1);
-            }
-            if (radians > 1.570779f && radians < 1.5708138f)
-            {
-                return (1, 0);
-            }
-            if (radians < -3.1415753f || radians > 3.1415753f)
-            {
-                return (0, -1);
-            }
-            if (radians > -1.5708138f && radians < -1.570779f)
-            {
-                return (-1, 0);
-            }
-            return MathF.SinCos(radians);
-        }
-
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static (float Sin, float Cos) SinCos(float radians)
--- tests/Pmad.Geometry.Benchmark/VectorOperations/SinCosBenchmark.cs
+++ tests/Pmad.Geometry.Benchmark/VectorOperations/SinCosBenchmark.cs
@@ -6,12 +6,10 @@ namespace Pmad.Geometry.Benchmark.VectorOperations
     public class SinCosBenchmark
     {
         [Benchmark] public object SinCos_Double_Alone() => Math.SinCos(1.0);
-        [Benchmark] public object SinCos_Double_V2() => MatrixHelper.SinCos(1.0);
-        [Benchmark] public object SinCos_Double_V1() => MatrixHelper.SinCosV1(1.0);
-        //[Benchmark] public void SinCos_Double_Dispatcher() => MatrixHelper.SinCos<double>(1.0);
+        [Benchmark] public object SinCos_Double() => MatrixHelper.SinCos(1.0);
+        [Benchmark] public void SinCos_Double_Dispatcher() => MatrixHelper.SinCos<double>(1.0);
         [Benchmark] public object SinCos_Float_Alone() => MathF.SinCos(1f);
-        [Benchmark] public object SinCos_Float_V2() => MatrixHelper.SinCos(1f);
-        [Benchmark] public object SinCos_Float_V1() => MatrixHelper.SinCosV1(1f);
-        //[Benchmark] public void SinCos_Float_Dispatcher() => MatrixHelper.SinCos<float>(1f);
+        [Benchmark] public object SinCos_Float() => MatrixHelper.SinCos(1f);
+        [Benchmark] public void SinCos_Float_Dispatcher() => MatrixHelper.SinCos<float>(1f);
     }
 }
